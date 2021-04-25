868987b0d46099965318dbe8ca20b76289f64648
samirrh <66929978+samirrh@users.noreply.github.com>
2021-04-25 15:41:49 -0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,3 +1,19 @@
+def insertion(arr):
+    for i in range(len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j>=0 and key<arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+def bubble(arr):
+    x = len(arr) - 1
+    for i in range(x):
+        for k in range(x-i):
+            if arr[k] > arr[k+1]:
+                arr[k], arr[k+1] = arr[k+1], arr[k]
+                
 def mergeSort(arr):
     
     if len(arr)>1:
